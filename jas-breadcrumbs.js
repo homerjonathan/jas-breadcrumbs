92c56d501912e91f2713bc925173ffd05ae2f9de
jhomer@jasoftware.com <jhomer@jasoftware.com>
2019-04-07 21:31:50 +0100

    Polymer 3

--- jas-breadcrumbs.js
+++ jas-breadcrumbs.js
@@ -1,18 +1,17 @@
-<link rel="import" href="../polymer/polymer-element.html">
-<link rel="import" href="../polymer/lib/mixins/gesture-event-listeners.html">
-<link rel="import" href="../paper-icon-button/paper-icon-button.html">
-<link rel="import" href="../iron-icon/iron-icon.html">
-<link rel="import" href="../iron-iconset-svg/iron-iconset-svg.html">
-<!--
+/**
 Element that does a simple Breadcrumbs Menu
 ##### Example
     <jas-breadcrumbs></jas-breadcrumbs>
 @element jas-breadcrumbs
 @status alpha
 @demo demo/index.html
--->
-<dom-module id="jas-breadcrumbs">
-    <template>
+*/
+
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+class JasBreadcrumbs extends Polymer.GestureEventListeners(Polymer.Element) {
+  static get template() {
+    return Polymer.html`
         <style>
             :host {
                 display: block;
@@ -60,8 +59,8 @@ Element that does a simple Breadcrumbs Menu
         </style>
         <iron-iconset-svg name="breadcrumbs_icons" size="24">
             <svg><defs>
-                <g id="help"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/></g>
-                <g id="chevron-right"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></g>
+                <g id="help"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"></path></g>
+                <g id="chevron-right"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></g>
             </defs></svg>
         </iron-iconset-svg>
         <div class="flex layout breadcrumb_layout">
@@ -71,45 +70,43 @@ Element that does a simple Breadcrumbs Menu
             </template>
             <paper-icon-button on-tap="_selectHelp" class="helpIcon" icon="breadcrumbs_icons:help"></paper-icon-button>
         </div>
-    </template>
-    <script>
-        class JasBreadcrumbs extends Polymer.GestureEventListeners(Polymer.Element) {
-            static get is() { return 'jas-breadcrumbs'; }       
-            static get properties()      {
-                return {
-                    menu: {
-                        type: Object,
-                        notifies: true
-                    },
-                    selected: {
-                        type: String,
-                        notifies: true,
-                        reflectToAttribute: true
-                    }
-                }
-            }
-            constructor() {
-                super();
-                JasBreadcrumbs.hook = this;
-            }
-            setMenu(newMenu) {
-                this.menu = newMenu;
-            }
-            _selectPage(e) {
-                var model = e.model;
-                if (model.m != undefined) {
-                    this.selected = model.m.id;
-                    this.fire('page_change', {page:  model.m.id});
-                }
-            }
-            _selectHelp() {
-                this.selected = "page_help";
-                this.fire('page_help',{ page: "help" });
-            }
-            fire(name,detail) {
-                document.dispatchEvent(new CustomEvent(name, {detail: detail}));
-            }
-        }    
-        customElements.define(JasBreadcrumbs.is, JasBreadcrumbs);
-    </script>
-</dom-module>
+`;
+  }
+
+  static get is() { return 'jas-breadcrumbs'; }
+  static get properties()      {
+      return {
+          menu: {
+              type: Object,
+              notifies: true
+          },
+          selected: {
+              type: String,
+              notifies: true,
+              reflectToAttribute: true
+          }
+      }
+  }
+  constructor() {
+      super();
+      JasBreadcrumbs.hook = this;
+  }
+  setMenu(newMenu) {
+      this.menu = newMenu;
+  }
+  _selectPage(e) {
+      var model = e.model;
+      if (model.m != undefined) {
+          this.selected = model.m.id;
+          this.fire('page_change', {page:  model.m.id});
+      }
+  }
+  _selectHelp() {
+      this.selected = "page_help";
+      this.fire('page_help',{ page: "help" });
+  }
+  fire(name,detail) {
+      document.dispatchEvent(new CustomEvent(name, {detail: detail}));
+  }
+}
+customElements.define(JasBreadcrumbs.is, JasBreadcrumbs);
